299217ce92f9d76c2421f2b4ee23b4e91ed63855
whqr <whqr@Noutbuk-Nikita.local>
2025-01-02 18:58:17 +0300

    теперь треки все треки проигрываются

--- spotify clone/src/components/DisplayAlbum.jsx
+++ spotify clone/src/components/DisplayAlbum.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Navbar from './NavBar'
 import { useParams } from 'react-router-dom'
 import { albumsData, assets, songsData } from '../assets/assets'
+import { PlayerContext } from '../context/PlayerContext'
  
 
 const DisplayAlbum = () => {
 
     const {id} = useParams()
     const albumData =  albumsData[id]
-
+    const { playWithId } = useContext(PlayerContext )
   return (
     <>
         <Navbar/>
@@ -36,8 +37,8 @@ const DisplayAlbum = () => {
         </div>
         <hr />
         {
-          songsData.map((item,index)=>(
-            <div key={index} className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
+          songsData.map((item,index) => (
+            <div onClick={() => playWithId(item.id)} key={index} className='grid grid-cols-3 sm:grid-cols-4 gap-2 p-2 items-center text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer'>
               <p className='text-white'>
                 <b className='mr-4 text-[#a7a7a7]'>{index + 1}</b>
                 <img className='inline w-10 mr-5' src={item.image} alt="" />
