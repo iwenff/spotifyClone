c3cc16ad479a8e3bc05c2aeaf8464bb7172d19ff
whqr <whqr@Noutbuk-Nikita.local>
2025-01-02 19:06:23 +0300

    обернул App в PlayerContextProvider

--- spotify clone/src/main.jsx
+++ spotify clone/src/main.jsx
@@ -3,17 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
+import PlayerContextProvider from './context/PlayerContext'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <playerContextProvider>
-      <BrowserRouter>
-
+    <BrowserRouter>
+     <PlayerContextProvider>
         <App />
-   
-      </BrowserRouter>
-    </playerContextProvider>
+      </PlayerContextProvider>
+    </BrowserRouter>
   </StrictMode>
 )
